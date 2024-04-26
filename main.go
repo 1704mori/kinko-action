0d87f6ab1961cb784981f4e2b070c3a91791c2df
1704mori <1704mori@gmail.com>
2024-04-25 21:39:44 -0300

    Add debug mode to action.yml and main.go

--- main.go
+++ main.go
@@ -34,10 +34,14 @@ func main() {
 		githubactions.Fatalf("error making request: %v", err)
 	}
 
+	debug := githubactions.GetInput("debug")
+
 	for _, secret := range res {
 		fmt.Printf("%+v\n", secret)
 		githubactions.SetEnv(secret.Key, secret.Value)
-		githubactions.AddMask(secret.Value)
+		if debug != "true" || debug == "" {
+			githubactions.AddMask(secret.Value)
+		}
 	}
 
 	githubactions.SetOutput("output", "done")
